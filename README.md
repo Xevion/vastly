17c35e59c5464e67fe0161fda908239b8540fcff
Xevion <xevion@xevion.dev>
2024-12-13 20:54:24 -0600

    wails project init

--- README.md
+++ README.md
@@ -23,3 +23,21 @@ What I want from this tool:
   - I would like each instance to have a time limit for my usage, both a soft and hard limit.
   - The soft limit will send me a message on Discord, and the hard limit will destroy the instance after another message.
   -
+
+## About
+
+This is the official Wails React-TS template.
+
+You can configure the project by editing `wails.json`. More information about the project settings can be found
+here: https://wails.io/docs/reference/project-config
+
+## Live Development
+
+To run in live development mode, run `wails dev` in the project directory. This will run a Vite development
+server that will provide very fast hot reload of your frontend changes. If you want to develop in a browser
+and have access to your Go methods, there is also a dev server that runs on http://localhost:34115. Connect
+to this in your browser, and you can call your Go code from devtools.
+
+## Building
+
+To build a redistributable, production mode package, use `wails build`.
---
+++ app.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"go.uber.org/zap"
+	"xevion.dev/quickvast/api"
+)
+
+// App struct
+type App struct {
+	ctx context.Context
+}
+
+// NewApp creates a new App application struct
+func NewApp() *App {
+	return &App{}
+}
+
+// startup is called when the app starts. The context is saved
+// so we can call the runtime methods
+func (a *App) startup(ctx context.Context) {
+	a.ctx = ctx
+}
+
+// Greet returns a greeting for the given name
+func (a *App) Greet(name string) string {
+	return fmt.Sprintf("Hello %s, It's show time!", name)
+}
+
+func old() {
+	logger, _ := zap.NewDevelopment()
+	defer logger.Sync()
+
+	sugar := logger.Sugar()
+	// Load .env file
+	if err := godotenv.Load(); err != nil {
+		sugar.Fatal(err)
+	}
+
+	// Get API key from environment
+	apiKey := os.Getenv("VASTAI_API_KEY")
+	if apiKey == "" {
+		sugar.Fatal("VASTAI_API_KEY not found in environment")
+	}
+
+	// Create client
+	client := api.NewClient(apiKey)
+
+	// Create search
+	search := api.NewSearch()
+	// search.CPUCores = api.Ge(8)
+
+	// Perform search
+	sugar.Infow("Searching", "search", search)
+	resp, err := client.Search(search)
+	if err != nil {
+		sugar.Fatal(err)
+	}
+
+	// Print offers
+	sugar.Infof("Offers: %d", len(resp.Offers))
+	for _, offer := range resp.Offers {
+		sugar.Info(offer.String())
+	}
+
+	sugar.Info("Done")
+}
